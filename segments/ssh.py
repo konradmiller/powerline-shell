4a915c38e4ac58aac1965dd443defb61e9112c33
Jens Kehne <jens@int80.de>
2013-12-01 17:51:25 +0100

    Revert ssh segment (i.e. re-add shells other than bash) and adapt for proxy.

--- segments/ssh.py
+++ segments/ssh.py
@@ -3,7 +3,18 @@ import os
 def add_ssh_segment():
 
     if os.getenv('SSH_CLIENT'):
-        str = powerline.network + " \\h ";
+        import socket
+
+        if powerline.shell == 'bash':
+            host_prompt = ' \\h '
+        elif powerline.shell == 'zsh':
+            host_prompt = ' %m '
+        else:
+            import socket
+            host_prompt = ' %s ' % socket.gethostname().split('.')[0]
+
+        str = powerline.network + host_prompt
+
         powerline.append(' %s ' % str, Color.SSH_FG, Color.SSH_BG)
 
-powerline.register( add_ssh_segment )
+powerline.register(add_ssh_segment)

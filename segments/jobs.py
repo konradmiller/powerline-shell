b917cc440082281481bc9fae3e582e14f93d1b3a
Konrad Miller <miller@kit.edu>
2013-12-03 15:08:41 +0100

    pass jobs variable instead of gathering info

--- segments/jobs.py
+++ segments/jobs.py
@@ -3,10 +3,10 @@ import re
 import subprocess
 
 def add_jobs_segment():
-    pppid = str(powerline.bashpid)
-    output = subprocess.Popen(['ps', '-a', '-o', 'ppid'], stdout=subprocess.PIPE).communicate()[0]
-    num_jobs = len(re.findall(str(pppid), output)) - 1
-
+#    pppid = str(powerline.bashpid)
+#    output = subprocess.Popen(['ps', '-a', '-o', 'ppid'], stdout=subprocess.PIPE).communicate()[0]
+#    num_jobs = len(re.findall(str(pppid), output)) - 1
+    num_jobs = powerline.jobs
     if num_jobs > 0:
         powerline.append(' %d ' % num_jobs, Color.JOBS_FG, Color.JOBS_BG)
 

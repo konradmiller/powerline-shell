1dc18df247e5c26488dc751bed0fffd4492635d4
Jens Kehne <jens@int80.de>
2013-12-04 15:31:32 +0100

    Show username segment in red if the current user is root

--- segments/username.py
+++ segments/username.py
@@ -8,7 +8,12 @@ def add_username_segment():
     else:
         user_prompt = ' %s ' % powerline.user
 
+    if powerline.user == 'root':
+        bgcolor = Color.USERNAME_ROOT_BG
+    else:
+        bgcolor = Color.USERNAME_BG
+
     if os.getenv('USER') != 'kmiller':
-        powerline.append(user_prompt, Color.USERNAME_FG, Color.USERNAME_BG)
+        powerline.append(user_prompt, Color.USERNAME_FG, bgcolor)
 
 powerline.register( add_username_segment )

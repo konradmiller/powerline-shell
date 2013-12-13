80d64ed5ab5f4bbdea9aa76d89338eca9308a754
Jens Kehne <jens@int80.de>
2013-12-13 22:57:27 +0100

    Don't show non-existent paths as read-only

--- segments/read_only.py
+++ segments/read_only.py
@@ -3,7 +3,7 @@ import os
 def add_read_only_segment():
     cwd = powerline.cwd or os.getenv('PWD')
 
-    if not os.access(cwd, os.W_OK):
+    if os.path.exists(cwd) and not os.access(cwd, os.W_OK):
         powerline.append(' %s ' % powerline.lock, Color.READONLY_FG, Color.READONLY_BG)
 
 powerline.register( add_read_only_segment )
